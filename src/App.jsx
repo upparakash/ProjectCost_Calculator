1042ba396e2db04c6ed83ef0e3b6da0d72e355cb
Chandana jada <chandanaj405@gmail.com>
2025-10-14 16:41:31 +0530

    edit app.jsx
    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,20 @@ import UiSelection from "./components/UiSelection";
 import SizeSelection from "./components/SizeSelection";
 import UserSelection from "./components/UserSelection";
 import UsersAndGenerated from "./components/UsersAndGenreated";
-
+import DatesAndLocation from "./components/DatesAndLocation";
+import  SocialAndEngagement from "./components/SocialAndEngagement";
+import BillingAndEcommerce from "./components/BillingAndEcommerce";
+import AdminFeedback from "./components/AdminFeedback";
 function App() {
   const [selectedPlatforms, setSelectedPlatforms] = useState([]);
   const [selectedSizes, setSelectedSizes] = useState([]);
   const [selectedUis, setSelectedUis] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState([]);
-  const [selectedGenerators, setSelectedGenerators] = useState([]); // added state
-
+  const [selectedGenerators, setSelectedGenerators] = useState([]);
+  const [selectedDates, setSelectedDates]=useState([]);
+  const [selectedEngagement,setSelectedEngagement]=useState([]);
+  const [selectedBilling,setSelectedBilling]=useState([]);
+  const [selectedAdmins,setSelectedAdmins]=useState([]);
   return (
     <div className="container">
       <h1 className="heading">HOW MUCH TO MAKE AN APP OR WEB?</h1>
@@ -48,14 +54,32 @@ function App() {
         selectedGenerators={selectedGenerators}
         setSelectedGenerators={setSelectedGenerators}
       />
-
-      {/* Sidebar */}
+      <DatesAndLocation
+      selectedDates={selectedDates}
+      setSelectedDates={setSelectedDates}
+      />
+      <SocialAndEngagement
+      selectedEngagement={selectedEngagement}
+      setSelectedEngagement={setSelectedEngagement}/>
+      <BillingAndEcommerce
+      selectedBilling={selectedBilling}
+      setSelectedBilling={setSelectedBilling}
+      />
+      < AdminFeedback
+      selectedAdmins={selectedAdmins}
+      setSelectedAdmins={setSelectedAdmins}
+      />
+        {/* Sidebar */}
       <Sidebar
         selectedPlatforms={selectedPlatforms}
         selectedSizes={selectedSizes}
         selectedUis={selectedUis}
         selectedUsers={selectedUsers}
         selectedGenerators={selectedGenerators}
+        selectedDates={selectedDates}
+        selectedEngagement={selectedEngagement}
+        selectedBilling={selectedBilling}
+        selectedAdmins={selectedAdmins}
       />
     </div>
   );
