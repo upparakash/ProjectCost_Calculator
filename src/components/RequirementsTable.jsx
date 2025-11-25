def2235801fbd88e8c85b9ba243e232a623713e3
Chandana jada <chandanaj405@gmail.com>
2025-11-25 14:21:53 +0530

    Update RequirementsTable.jsx

--- src/components/RequirementsTable.jsx
+++ src/components/RequirementsTable.jsx
@@ -199,7 +199,8 @@ const RequirementsTable = ({
   const addStyledFooter = (pdf, tableBottomY) => {
     const pageWidth = pdf.internal.pageSize.getWidth();
     const pageHeight = pdf.internal.pageSize.getHeight();
-    const footerY = pageHeight - 70;
+   const footerY = Math.min(pageHeight - 70, tableBottomY + 140);
+
 
     pdf.setFillColor(59, 130, 246);
     pdf.rect(0, footerY, pageWidth, 60, "F");
